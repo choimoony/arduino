a0ca17b0cbaf1c0897d0f08344d280bbb5b060ef
moony <57083595+choimoony@users.noreply.github.com>
2022-01-03 14:02:59 +0900

    Add files via upload

---
+++ 9_Servo/9_Servo.ino
@@ -0,0 +1,28 @@
+#include <Servo.h> 
+ 
+int servoPin = 6;
+int angle = 0; 
+
+Servo servo; 
+
+void setup() 
+{ 
+    servo.attach(servoPin); 
+} 
+
+
+void loop() 
+{ 
+
+  for(angle = 0; angle < 180; angle++) 
+  { 
+    servo.write(angle); 
+    delay(15); 
+  } 
+  
+  for(angle = 180; angle > 0; angle--) 
+  { 
+    servo.write(angle); 
+    delay(15); 
+  } 
+} 
